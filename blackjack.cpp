fd9b152136f09a4b61a666dac042cc4aba84b9d7
Yake <yakethesnake3230@gmail.com>
2012-12-02 15:56:06 -0800

    Refined Hand class
    Hand class has replaced Card class

--- blackjack.cpp
+++ blackjack.cpp
@@ -15,58 +15,16 @@ int main() {
 	srand (time(NULL));
 	// const int winValue = 21;
 	// Game loop ends if it's 'y'
-	// char endGame = 'n';
-	yHand.dealCard(2);
-	yHand.displayHand();
+	char continue = 'y';
 
-	// cout << "=======================================================\n" <<
-	// 		"  XXXXX                  XX                XXX      X\n" <<
-	// 		" X     X                  X               X   X   XXX\n" <<
-	// 		" X                        X                   X     X\n" <<
-	// 		" X       XX XX    XXXX    X  XX   XXXXX       X     X\n" <<
-	// 		"  XXXXX   XX  X       X   X  X   X     X     X      X\n" <<
-	// 		"       X  X   X   XXXXX   X X    XXXXXXX    X       X\n" <<
-	// 		"       X  X   X  X    X   XXX    X         X        X\n" <<
-	// 		" X     X  X   X  X    X   X  X   X     X  X         X\n" <<
-	// 		"  XXXXX  XXX XXX  XXXX X XX   XX  XXXXX   XXXX  XXXXX\n" <<
-	// 		"=======================================================\n" <<
-	// 		"Simple Blackjack (21) by Jacob Williams\n" <<
-	// 		"Gameplay:\n" <<
-	// 		"> Single player\n" <<
-	// 		"> Unlimited Decks\n" <<
-	// 		"> Only allows for hit or stand\n" <<
-	// 		"> House hits below 17\n" <<
-	// 		"> No specialized rules\n" <<
-	// 		"> No chips or money involved\n" <<
-	// 		"=======================================================\n\n" <<
-	// 		"Want to play a hand?(y/n): ";
-	// cin >> endGame;
-	// cout << endl;
-	// while (endGame == 'y') {
-		
-	// 	newHand();
+	while (continue == 'y') {
+		// first hand
+		yHand.dealCard(2);
+		hHand.dealCard(1);
 
-	// 	cout << "====================== New Hand =======================\n\n";
-	// 	char stand = 'h';
-	// 	while (stand == 'h') {
-	// 		cout << "Your Hand: 10(Club) King(Diamond) = 20\n";
-	// 		cout << "House Hand: 10(Club) King(Diamond) = 20\n\n";
-	// 		cout << "Hit or Stand(h/s): ";
-	// 		cin >> stand;
-	// 	}
-	// 	if (yScore > winValue) {
-	// 		cout << "You busted, I'm sorry. Play another hand? (y/n): ";
-	// 	} else if (yScore > hScore) {
-	// 		cout << "You Won, that's great. Another hand?(y/n): ";
-	// 	} else if (yScore < hScore) {
-	// 		cout << "The house won, I'm sorry. Play another hand? (y/n): ";
-	// 	} else {
-	// 		cout << "You tied the house. Another hand?(y/n): ";
-	// 	}
-	// 	cin >> endGame;
-	// 	cout << "\n";
-	// }
-	// cout << "Wins: / Losses: \n" <<
-	// 		"=============XXXX Thanks for playing! XXXX=============\n";
+		cout << "Your Hand: " << yHand.displayHand() << " = " << yHand.getScore() << endl;
+	 	cout << "House Hand: " << hHand.displayHand() << "Unkown(?) = " << hHand.getScore() << endl;
+	}
+	
 	return 0;
 }
--- hand.cpp
+++ hand.cpp
@@ -1,24 +1,45 @@
-class hand {
+class Hand {
 	vector<int> cards;
 public:
 	void dealCard(int amount);
 	void displayHand();
+	int getScore();
 }yHand, hHand;
 
-void dealCard(unsigned amount) {
+void Hand::dealCard(int amount) {
+	// Number of cards in a deck
+	const int DECK = 52;
+	int card;
 	// Deal cards to someone up to the 'amount' parameter
-	for (unsigned i=0;i<amount;i++) {
-		// Number of cards in a deck
-		const int DECK = 52;
+	for (int i=0;i<amount;i++) {
 		// Set card to random number out of DECK
-		int card = rand()%DECK+1;
+		card = rand()%DECK+1;
 		// Add card(int) to hand(vector)
 		cards.push_back(card);
 	}
 }
 
-void displayHand() {
-	for (unsigned i=0;i<cards.size();i++) {
-		cout << cards[1] << endl;
+void Hand::displayHand() {
+	for (int i=0;i<cards.size();i++) {
+		cout << cards[i] << endl;
 	}
+}
+
+int Hand::getScore() {
+	int total = 0;
+	for (int i=0;i<cards.size();i++) {
+		// if its a face card, value should be set to 10
+		if (cards[i]%13 == 0 or cards[i]%13 > 10) {
+			total += 10;
+		}
+		// What if it's an ace?
+		else if (cards[i]%13 == 1) {
+			total += 11;
+		}
+		// Otherwise, we just want its number value
+		else {
+			total += cards[i]%13;
+		}
+	}
+	return total;
 }
